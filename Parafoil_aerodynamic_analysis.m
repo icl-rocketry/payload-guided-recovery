29fbbd618028499cbdf07f67bcdc4e9a0d3f5509
Ishan <99824864+aZira371@users.noreply.github.com>
2024-09-24 04:48:40 +0100

    Calculations related to coefficients for six dof
    Results have been stored in the excel called aerodynamic_analysis_results.xlsx. C_M2 and up_C_M2 equations need to be adjusted a bit.

--- Parafoil_aerodynamic_analysis.m
+++ Parafoil_aerodynamic_analysis.m
@@ -24,6 +24,11 @@ alpha = [-7:0.05:10] .* pi/180;
 for i = 1:length(a)
     C_L(:,i) = a(i) .* (alpha - alpha_zl);
 end
+% Lift coefficient calculation
+for i = 1:length(a)
+    C_Lalpha(:,i) = a(i);
+end
+
 
 % Plotting C_L vs alpha
 figure();
@@ -62,15 +67,40 @@ legend('AR = 3', 'AR = 3.3');
 hold off
 
 % Span and line length ratio calculations
-b = 1.42; % Span (m)
+b = 1.421; % Span (m)
 R_b = linspace(0.4, 1.6, length(alpha));
 R = b .* R_b;
 beta = b ./ (4 .* R);
-alpha = 5 * pi/180;
+% alpha = 5 * pi/180;
 
 for i = 1:length(a)
     C_L2(:,i) = a(i) .* (alpha - alpha_zl) .* cos(beta).^2;
 end
+% Plotting C_L2 vs alpha
+figure();
+xline(0);
+hold on
+plot(alpha .* 180/pi, C_L2(:,1));
+hold on
+plot(alpha .* 180/pi, C_L2(:,2));
+xlabel('alpha');
+ylabel('C_{L2}');
+legend('alpha = 0','AR = 3', 'AR = 3.3');
+hold off
+for i = 1:length(a)
+    C_L2alpha(:,i) = a(i).* cos(beta).^2;
+end
+% Plotting C_L2alpha vs alpha
+figure();
+plot(alpha .* 180/pi, C_L2alpha(:,1));
+hold on
+plot(alpha .* 180/pi, C_L2alpha(:,2));
+hold on
+xline(0);
+xlabel('alpha');
+ylabel('C_{L2_{alpha}}');
+legend('AR = 3', 'AR = 3.3', 'alpha = 0');
+hold off
 
 % Plotting C_L2/C_L vs R/b
 figure();
@@ -86,8 +116,8 @@ hold off
 S = b^2 ./ AR;
 c = b ./ AR;
 d = 1.5e-3; % Line diameter (m)
-n = 12;
-A_cube = 0.1 * 0.1; % Cubesat area (m^2)
+n = 24;
+A_cube = 0.3 * 0.1; % Cubesat area (m^2)
 
 for i = 1:length(a)
     C_Dl(:,i) = (n .* R .* d .* cos(alpha).^3) ./ S(i);
@@ -97,6 +127,15 @@ end
 for i = 1:length(delta)
     C_D2(:,i) = C_D0 + C_Dl(:,i) + C_Ds(:,i) + (C_L(:,i).^2 .* (1 + delta(i))) ./ (pi * AR(i));
 end
+% Plotting C_L2/C_D2 vs R/b
+figure();
+plot(alpha*180/pi, C_L2(:,1) ./ C_D2(:,1));
+hold on
+plot(alpha*180/pi, C_L2(:,2) ./ C_D2(:,2));
+xlabel('alpha(deg)');
+ylabel('C_L/C_D');
+legend('AR = 3', 'AR = 3.3');
+hold off
 
 % Plotting C_L2/C_D2 vs R/b
 figure();
@@ -108,6 +147,101 @@ ylabel('C_L/C_D');
 legend('AR = 3', 'AR = 3.3');
 hold off
 
+% Plotting C_D2 vs alpha
+figure();
+xline(0);
+hold on
+plot(alpha .* 180/pi, C_D2(:,1));
+hold on
+plot(alpha .* 180/pi, C_D2(:,2));
+xlabel('alpha');
+ylabel('C_{D2}');
+legend('alpha = 0','AR = 3', 'AR = 3.3');
+hold off
+
+for i = 1:length(a)
+    C_Dlalpha(:,i) = -(n .* R .* d .*3.* (alpha).^2.*sin(alpha).^3) ./ S(i);
+end
+
+for i = 1:length(delta)
+    C_D2alpha(:,i) =  C_Dlalpha(:,i) + (2.*C_L(:,i).*C_Lalpha(:,i) .* (1 + delta(i))) ./ (pi * AR(i));
+end
+% Plotting C_D2alpha vs alpha
+figure();
+xline(0);
+hold on
+plot(alpha .* 180/pi, C_D2alpha(:,1));
+hold on
+plot(alpha .* 180/pi, C_D2alpha(:,2));
+xlabel('alpha');
+ylabel('C_{D2_{alpha}}');
+legend('alpha = 0','AR = 3', 'AR = 3.3');
+hold off
+
+% C_M calculations
+% maintaining stable equilibrium at each alpha
+rho = 1.225; % Air density (kg/m^3)
+R_b_dp = 0.6; % Optimum based on the last figure
+R_dp = R_b_dp * b;
+m_s = 3; 
+g = 9.81; 
+C_Mc4 = 0;
+mu = [-100:0.05:100] .* pi/180; 
+corr_mu = NaN(1, length(alpha)); 
+
+for i = 1:length(alpha)
+    gamma(:,i) = atan(1/(C_L2(i,2)/C_D2(i,2)));
+    Wt = 3 * 9.81; 
+    Dr = sqrt(C_L2(i,2)^2 + C_D2(i,2)^2);
+    V = sqrt((2 * Wt) / (rho * S(2) * Dr));
+    LbyD_alpha(i) = (C_L2alpha(i,2) * C_D2(i,2) - C_L2(i,2) * C_D2alpha(i,2)) / (C_D2(i,2)^2);
+    d_gamma_alpha = (1 / (1 + (1 / (C_L2(i,2) / C_D2(i,2)))^2)) * LbyD_alpha(i);
+    for j = 1:length(mu)
+        C_M2(j,i) = C_Mc4 - (R_dp/c(2)) * (C_Ds(2) * cos(alpha(i) + mu(j))) ...
+            - R_dp * n * R_dp * d * (cos(alpha(i) + mu(j))^2) / (S(2) * 2 * c(2)) ...
+            - m_s * g * R_dp * sin(alpha(i) + mu(j) - gamma(:,i)) / (0.5 * rho * V^2 * S(2) * c(2)); 
+
+        [~, idx] = min(abs(C_M2(:,i))); 
+        corr_mu(i) = mu(idx) * 180/pi; 
+        corr_mu(i) = corr_mu(i) * pi/180;
+        C_M2_alpha(i) = (R_dp/c(2)) * (C_Ds(2) * sin(alpha(i) + corr_mu(i))) ...
+            + R_dp * n * R_dp * d * (sin(alpha(i) + corr_mu(i))^2) / (S(2) * 2 * c(2)) ...
+            - d_gamma_alpha * m_s * g * R_dp * cos(alpha(i) + corr_mu(i) - gamma(:,i)) / (0.5 * rho * V^2 * S(2) * c(2));
+        % Penalize if C_M2_alpha is not negative
+        if C_M2_alpha >= 0
+            corr_mu(i) = Inf; 
+        end
+    end
+end
+% Plotting stable equilibrium mu vs alpha
+figure();
+xline(0);
+hold on
+plot(alpha .* 180/pi, corr_mu.*180/pi);
+xlabel('alpha');
+ylabel('rigging angle for stable equilibrium');
+hold off
+%updating C_M
+for i =1:length(alpha)
+    up_C_M2(i) = C_Mc4 - (R_dp/c(2)) * (C_Ds(2) * cos(alpha(i) + corr_mu(i))) ...
+            - R_dp * n * R_dp * d * (cos(alpha(i) + corr_mu(i))^2) / (S(2) * 2 * c(2)) ...
+            - m_s * g * R_dp * sin(alpha(i) + corr_mu(i) - gamma(:,i)) / (0.5 * rho * V^2 * S(2) * c(2)); 
+end
+% Plotting updated C_M2 vs alpha
+figure();
+xline(0);
+hold on
+plot(alpha .* 180/pi, up_C_M2);
+xlabel('alpha');
+ylabel('updated C_{M2}');
+legend('alpha = 0','C_{M2}')
+hold off
+
+headers = {'alpha (deg)', 'C_L2', 'C_L2alpha', 'C_D2', 'C_D2alpha', 'up_C_M2', 'C_M2_alpha','corr_mu'};
+data = [alpha' .* 180/pi, C_L2(:,2), C_L2alpha(:,2), C_D2(:,2), C_D2alpha(:,2), up_C_M2', C_M2_alpha',corr_mu' .*180/pi]; 
+filename = 'aerodynamic_analysis_results.xlsx';
+writecell(headers, filename, 'Sheet', 1, 'Range', 'A1');
+writematrix(data, filename, 'Sheet', 1, 'Range', 'A2');
 %% Design points
 
 R_b_dp = 0.6; % Optimum based on the last figure
@@ -136,25 +270,26 @@ w = V * sin(gamma);
 m_s = 3; % Mass (kg)
 g = 9.81; % Gravity (m/s^2)
 C_Mc4 = 0;
-mu1 = [-10:1:15] .* pi/180;
+mu1 = [-10:2:15] .* pi/180;
 alpha1 = [-7:0.05:30] .* pi/180;
 
 for j = 1:length(mu1)
     for i = 1:length(alpha1)
-        C_M(j,i) = C_Mc4 - R_dp * (C_Ds_dp * cos(alpha1(i) + mu1(j))) - R_dp * n * R_dp * d * (cos(alpha1(i) + mu1(j))^2) / (S(2) * 2 * c(2)) - m_s * g * R_dp * sin(alpha1(i) + mu1(j) - gamma) / (0.5 * rho * V^2 * S(2) * c(2));
+        C_M(j,i) = C_Mc4 - (R_dp/c(2)) * (C_Ds_dp * cos(alpha1(i) + ...
+            mu1(j))) - R_dp * n * R_dp * d * (cos(alpha1(i) + mu1(j))^2) / (S(2) * 2 * c(2)) - m_s * g * R_dp * sin(alpha1(i) + mu1(j) - gamma) / (0.5 * rho * V^2 * S(2) * c(2));
     end
 end
 
 % Plotting rigging angle analysis
 y = zeros(size(alpha1));
-x = alpha_dp * ones(size(alpha1));
+x = (alpha_dp*180/pi) * ones(size(alpha1));
 figure();
-plot(alpha1, y);
+plot(alpha1.*180/pi, y);
 hold on
 plot(x, alpha1);
 hold on
 for j = 1:length(mu1)
-    plot(alpha1, C_M(j,:));
+    plot(alpha1.*180/pi, C_M(j,:));
 end
 xlabel('alpha (deg)');
 ylabel('C_M');
